431eb21a2f0bc7e5756cc99bf445aa41e436f091
nyxeet <nyxeet@ukr.net>
2020-12-02 21:15:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,8 +14,7 @@ const stopButtonRef = document.querySelector('button[data-action="stop"]');
 const bodyRef = document.querySelector('body')
 
 startButtonRef.addEventListener('click', event => { backgroundGenerator.start() })
-stopButtonRef.addEventListener('click', event => {backgroundGenerator.stop()
-})
+stopButtonRef.addEventListener('click', event => {backgroundGenerator.stop()})
 
 const backgroundGenerator = {
     isActive: false,
@@ -24,13 +23,9 @@ const backgroundGenerator = {
         if (this.isActive) {
             return;
         }
-        console.log(this)
         this.isActive = true;
-        const setBodyBackgroundColor = () => {
-        bodyRef.setAttribute("style", "background:" + colors[randomIntegerFromInterval(0, colors.length - 1)] + ";")
-        }
         this.intervalId = setInterval(setBodyBackgroundColor, 1000)
-        console.log(this.intervalId)
+
     },
     stop() {
         clearInterval(this.intervalId);
@@ -41,7 +36,10 @@ const backgroundGenerator = {
 }
 
 
-
+const setBodyBackgroundColor = () => {
+        bodyRef.setAttribute("style", "background:" + colors[randomIntegerFromInterval(0, colors.length - 1)] + ";")
+}
+        
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
